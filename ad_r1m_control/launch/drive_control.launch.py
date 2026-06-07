b340faa22cb90d6e5bfd4fbc67ba139cc699f021
Ioan Dragomir <ioan.dragomir@analog.com>
2026-06-07 18:33:07 +0300

    ad_r1m_{bringup,control}: Add option to disable sensor fusion
    $ ros2 launch ad_r1m_bringup bringup.launch.py sensor_fusion:=false
    
    Signed-off-by: Ioan Dragomir <ioan.dragomir@analog.com>

--- ad_r1m_control/launch/drive_control.launch.py
+++ ad_r1m_control/launch/drive_control.launch.py
@@ -2,15 +2,15 @@ import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, IfElseSubstitution
 from launch_ros.substitutions import FindPackageShare
-
 
 def generate_launch_description():
     rosdistro = os.environ['ROS_DISTRO']
     ad_r1m_control = FindPackageShare('ad_r1m_control')
 
     decl_extra_control_params_file = DeclareLaunchArgument('extra_control_params_file', default_value='')
+    decl_sensor_fusion = DeclareLaunchArgument('sensor_fusion', default_value='true')
 
 
     # humble-and-earlier use cmd_vel_unstamped, jazzy-and-later use cmd_vel
@@ -35,11 +35,17 @@ def generate_launch_description():
     robot_control_config = PathJoinSubstitution([
         ad_r1m_control, 'config', 'controllers.yaml'
     ])
+    no_sensor_fusion_config = PathJoinSubstitution([
+        ad_r1m_control, 'config', 'controllers_no_ekf.yaml'
+    ])
     controller_manager_node = Node(
         package='controller_manager',
         executable='ros2_control_node',
         parameters=[
             robot_control_config,
+            IfElseSubstitution(LaunchConfiguration('sensor_fusion'),
+                               if_value='',
+                               else_value=no_sensor_fusion_config),
             LaunchConfiguration('extra_control_params_file')
         ],
         remappings=[('~/robot_description', 'robot_description')],
