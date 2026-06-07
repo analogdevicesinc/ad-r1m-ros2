998146ac4461b3889bc0609b1f406600aa528535
Ioan Dragomir <ioan.dragomir@analog.com>
2026-06-07 18:06:25 +0300

    system: rpi5: tof.sh: Add chrony time sync step
    Run chronyc makestep, burst 4/8 to make sure the ToF clock is
    synchronized to the host. Fixes sporadic errors in the ToF pipeline.
    
    Signed-off-by: Ioan Dragomir <ioan.dragomir@analog.com>

--- system/rpi5/usr/lib/ad-r1m/tof.sh
+++ system/rpi5/usr/lib/ad-r1m/tof.sh
@@ -2,7 +2,10 @@
 
 # Restart ToF software
 ping -c2 -w2 192.168.56.1 && {
-	echo "Restarting ToF software"
+	echo "ToF: Synchronizing clock"
+	echo analog | sshpass -panalog ssh analog@192.168.56.1 "sudo -S chronyc makestep"
+	echo analog | sshpass -panalog ssh analog@192.168.56.1 "sudo -S chronyc burst 4/8 192.168.56.10"
+	echo "ToF: Restarting ROS 2 node"
 	echo analog | sshpass -panalog ssh analog@192.168.56.1 "sudo -S systemctl restart ros2-docker-app.service"
 } || {
 	echo "Couldn't reach ToF camera"
